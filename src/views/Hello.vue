36e9c775cecb9fed1b49e5fb7e5fc4547c398eea
alex <alex.kornatiy@gmail.com>
2019-01-17 17:04:39 +0200

    Added Table, with inf about Person

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -1,23 +1,31 @@
 <template>
   <el-main>
-    <el-table :data="tableData">
-      <el-table-column prop="date" label="Date" width="140"/>
-      <el-table-column prop="name" label="Name" width="120"/>
-      <el-table-column prop="some_inf" label="Some information"/>
+    <el-table :data="person">
+      <el-table-column prop="name" label="Name" width="140"/>
+      <el-table-column prop="password" label="Password" width="120"/>
+      <el-table-column prop="old" label="Old" width="120"/>
+      <el-table-column prop="Email" label="Email" width="120"/>
     </el-table>
   </el-main>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
-    const item = {
-      date: 'None',
-      name: 'John',
-      some_inf: 'None'
-    }
+    this.getPerson()
     return {
-      tableData: Array(1).fill(item)
+      person: []
+    }
+  },
+  methods: {
+    getPerson: function() {
+      const url = 'http://localhost:8080/api/names'
+      axios.get(url, {
+        dataType: 'json',
+        'Access-Control-Allow-Origin': 'localhost:8080',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE' })
+        .then(response => (this.person = response.data))
     }
   }
 }
