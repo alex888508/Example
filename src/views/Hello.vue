e6fb5a65b5dada5036b83eb9b7bb691455c284fb
alex <alex.kornatiy@gmail.com>
2019-01-21 02:57:08 +0200

    Added a button "Add person" (POST method)

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -6,6 +6,30 @@
       <el-table-column prop="old" label="Old" width="120"/>
       <el-table-column prop="Email" label="Email" width="120"/>
     </el-table>
+
+    <!-- Form -->
+    <el-button type="info" @click="dialogFormVisible = true">Add a person</el-button>
+
+    <el-dialog :visible.sync="dialogFormVisible" title="Add Person">
+      <div style="margin: 20px;"/>
+      <el-form :model="new_person" label-width="140px">
+        <el-form-item label="Person name">
+          <el-input v-model="new_person.name"/>
+        </el-form-item>
+        <el-form-item label="Person password">
+          <el-input v-model="new_person.password"/>
+        </el-form-item>
+        <el-form-item label="Person old">
+          <el-input v-model="new_person.old"/>
+        </el-form-item>
+        <el-form-item label="Person Email">
+          <el-input v-model="new_person.Email"/>
+      </el-form-item></el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false, postPerson()" >Add</el-button>
+      </span>
+    </el-dialog>
   </el-main>
 </template>
 
@@ -15,7 +39,14 @@ export default {
   data() {
     this.getPerson()
     return {
-      person: []
+      person: [],
+      dialogFormVisible: false,
+      new_person: {
+        id: '',
+        Email: '',
+        password: '',
+        old: ''
+      }
     }
   },
   methods: {
@@ -26,6 +57,15 @@ export default {
         'Access-Control-Allow-Origin': 'localhost:8080',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE' })
         .then(response => (this.person = response.data))
+    },
+    postPerson: function() {
+      const url = 'http://localhost:8080/api/names/users'
+      axios.post(url, this.new_person, {
+        dataType: 'json',
+        'Access-Control-Allow-Origin': 'localhost:8080',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+      })
+        .then(response => {})
     }
   }
 }
